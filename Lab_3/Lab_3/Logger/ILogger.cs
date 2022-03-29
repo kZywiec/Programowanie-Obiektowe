4b2359118ec168bc4461292737803d15f8cdfb2c
kZywiecPC <92109351+kZywiec@users.noreply.github.com>
2022-03-29 22:03:30 +0200

    Lab3

---
+++ Lab_3/Lab_3/Logger/ILogger.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace ConsoleApp.Logger
+{
+    public interface ILogger : IDisposable
+    {
+        void Log(params String[] messages);
+    }
+}
---
+++ Lab_3/Lab_3/Logger/WriterLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp.Logger
+{
+    public abstract class WriterLogger : ILogger
+    {
+        protected TextWriter writer;
+
+        public virtual void Log(params string[] messages)
+        {
+            // Uzupełnić to miejsce o logikę zapisu opartą o TextWriter ...
+        }
+
+        public abstract void Dispose();
+    }
+}
