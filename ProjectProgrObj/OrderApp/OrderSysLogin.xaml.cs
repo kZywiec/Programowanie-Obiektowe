1fb141ada0683280fb99edd4bcd2bd179f2550ac
kZywiecPC <92109351+kZywiec@users.noreply.github.com>
2022-06-06 22:39:57 +0200

    Made Login page. To fix DB connection

---
+++ ProjectProgrObj/OrderApp/OrderSysLogin.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OrderApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy OrderSysLogin.xaml
+    /// </summary>
+    public partial class OrderSysLogin : Page
+    {
+        private string LoginTemp = "";
+        public OrderSysLogin()
+        {
+            InitializeComponent();
+        }
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = @"DESKTOP-V81EUI2\SQLPROGOBJ";
+            using (DataContext dataContext = new DataContext(connectionString)) ;
+        }
+        private delegate bool LoginManipulationHolder();
+        private event LoginManipulationHolder ChangeLoginTemp;
+
+        //Funkcja wywołuje event zmiany na zmiennej LoginTemp
+        //sprawdzając czy użytkownik nie wprowadził zadużo znaków
+        protected virtual bool OnLoginTempChanged()
+        {
+            if (ChangeLoginTemp != null) 
+                ChangeLoginTemp();   
+            else if(LoginTemp.Length > 4)
+                return true;
+            return false;
+        }
+        //Wprowadzenie znaku do Tymczasowej zmiennej odpowiadającej za Login 
+        private void AddChar(char x)
+        {
+            this.LoginTemp += x.ToString(); 
+            OnLoginTempChanged(); 
+            if (OnLoginTempChanged()) 
+                (this.LoginTemp, LengthCounter.Content) = ("" + x, "");
+            LengthCounter.Content += "* "; //this.LoginTemp;
+        }
+        private void NumberButton0_Click(object sender, RoutedEventArgs e) => AddChar('0');
+        private void NumberButton1_Click(object sender, RoutedEventArgs e) => AddChar('1');
+        private void NumberButton2_Click(object sender, RoutedEventArgs e) => AddChar('2');
+        private void NumberButton3_Click(object sender, RoutedEventArgs e) => AddChar('3');
+        private void NumberButton4_Click(object sender, RoutedEventArgs e) => AddChar('4');
+        private void NumberButton5_Click(object sender, RoutedEventArgs e) => AddChar('5');
+        private void NumberButton6_Click(object sender, RoutedEventArgs e) => AddChar('6'); 
+        private void NumberButton7_Click(object sender, RoutedEventArgs e) => AddChar('7');
+        private void NumberButton8_Click(object sender, RoutedEventArgs e) => AddChar('8');
+        private void NumberButton9_Click(object sender, RoutedEventArgs e) => AddChar('9');
+    }
+}
---
+++ ProjectProgrObj/OrderApp/Workers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.Linq.Mapping;
+
+namespace OrderApp
+{
+    [Table(Name = "Users")]
+    public class UserEntity
+    {
+        [Column(IsPrimaryKey = true, Name = "Pin")]
+        public string Id { get; set; }
+
+        [Column(Name = "Pin")]
+        public string Pin { get; set; }
+
+        [Column(Name = "Name")]
+        public string Name { get; set; }
+
+        [Column(Name = "Second_Name")]
+        public string Second_Name { get; set; } 
+
+        [Column(Name = "Role_id")] 
+        public int Role_id { get; set; }
+
+        [Column(Name = "Pesel")]
+        public string Pesel { get; set; }
+
+        [Column(Name = "Phone_number")]
+        public string Phone_number { get; set; }
+    }
+}
